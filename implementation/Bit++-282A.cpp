51420ac5fc3f6759a36dd3b2c7f00a4bf730dae4
Jaya Prakash <jayaprakashgujjari79@gmail.com>
2026-07-14 11:56:39 +0530

    282A done

--- implementation/Bit++-282A.cpp
+++ implementation/Bit++-282A.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 int main()
 {
-    cout<<"hello"<<endl;
+    int n;
+ cin>>n;
+ int x=0;
+    while(n--)
+    { 
+        
+        string s;
+        for(int i=0;i<3;i++)
+        {
+            char ch;
+            cin>>ch;
+            s.push_back(ch);
+        }
+        string f=s.substr(0,2);
+        string l=s.substr(1,2);
+        if(f=="++"||l=="++")
+        {
+            x++;
+        } 
+        else if(f=="--"||l=="--")
+        {
+            x--;
+        }
+        
+    }
+    cout<<x<<endl;
 }
