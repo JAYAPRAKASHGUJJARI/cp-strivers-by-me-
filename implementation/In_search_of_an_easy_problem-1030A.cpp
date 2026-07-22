1cde1bcd29fbc0f174cda59313fdde0bd1a52401
Jaya Prakash <jayaprakashgujjari79@gmail.com>
2026-07-22 23:36:24 +0530

    in search of aneasy problem

---
+++ implementation/In_search_of_an_easy_problem-1030A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    bool hard=false;
+    for(int i=0;i<n;i++)
+    {
+        int op;
+        cin>>op;
+        if(op==1) 
+        {
+            hard=true;
+            break;}
+    }
+    if(hard) cout<<"HARD"<<endl;
+    else cout<<"EASY"<<endl;
+}
